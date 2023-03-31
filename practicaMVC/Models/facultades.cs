aa925c00d0a1edc484adc37fa2fa5d2b49b14b45
1234ro1 <josue.ramirez@catolica.edu.sv>
2023-03-30 18:51:03 -0600

    Agregar archivos de proyecto.

---
+++ practicaMVC/Models/facultades.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace practicaMVC.Models
+{
+    public class facultades
+    {
+        [Key]
+        public int facultad_id { get; set; }
+        public string nombre_facultad { get; set; }
+    }
+}
