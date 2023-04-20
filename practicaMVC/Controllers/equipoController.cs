c778ce89c365fca7fc8951b5d67b1e3ccdf6ea85
1234ro1 <josue.ramirez@catolica.edu.sv>
2023-04-20 16:00:27 -0600

    formulario

---
+++ practicaMVC/Controllers/equipoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace practicaMVC.Controllers
+{
+    public class equipoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
